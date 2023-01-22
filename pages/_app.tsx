2899e4d5bf8a9bf7797f29e7d05709f11d203c9c
Micha de Vries <micha@devrie.sh>
2023-01-22 17:54:51 +0100

    Add query client to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import Head from '../components/Head';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
             {/* We can place some default head params here, nested Head elements will overwrite it. */}
             <Head />
             <Component {...pageProps} />
-        </>
+        </QueryClientProvider>
     );
 }
