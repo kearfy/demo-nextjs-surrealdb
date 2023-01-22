f7f74e18fd73efd66ea7c3797470cdc8facdfd65
Micha de Vries <micha@devrie.sh>
2023-01-22 17:40:57 +0100

    Basic components

---
+++ components/RenderPost.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Post } from '../constants/Types';
+
+export default function RenderPost(post: Post) {
+    return (
+        <div className="flex flex-col gap-2 bg-zinc-200 rounded-3xl py-10 px-12">
+            <h2 className="text-4xl">{post.title}</h2>
+            <p className="text-zinc-600 pb-2">
+                By {post.author}, {post.created.getDate()}-
+                {post.created.getMonth() + 1}
+            </p>
+            <p>{post.body}</p>
+        </div>
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Index() {
-    return <p>Hello world!</p>;
+    return (
+        <div className="flex flex-col gap-8 p-16">
+            <h1>Hello world!</h1>
+        </div>
+    );
 }
